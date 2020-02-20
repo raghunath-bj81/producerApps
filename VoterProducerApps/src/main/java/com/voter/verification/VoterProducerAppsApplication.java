c164209670bde7f8c697e55182b7e36ca1f6395a
rjosula <rjosula@localhost>
2020-02-20 17:32:43 +0530

    Producer application which will produce an XML message to consumer.

---
+++ VoterProducerApps/src/main/java/com/voter/verification/VoterProducerAppsApplication.java
@@ -0,0 +1,13 @@
+package com.voter.verification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoterProducerAppsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoterProducerAppsApplication.class, args);
+	}
+
+}
